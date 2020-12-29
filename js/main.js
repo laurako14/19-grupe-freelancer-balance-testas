35b09d0733d01f9fecbb498855d6ff0c898cfc26
LauraKo <kovarskyte@yahoo.com>
2020-12-29 17:21:48 +0200

    Final be rusiavimo

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 import { RenderBalance } from "./renderBalance/RenderBalance.js";
 import { tableData } from "./data.js";
 
-new RenderBalance('.table-content', tableData, '.table-footer');
+new RenderBalance('.table-content', tableData, '.table-footer', '#minIncome', '#maxIncome', '#minExpenses', '#maxExpenses');
--- js/renderBalance/RenderBalance.js
+++ js/renderBalance/RenderBalance.js
@@ -1,7 +1,11 @@
 class RenderBalance {
-    constructor(selector, data, selector2) {
+    constructor(selector, data, selector2, selector3, selector4, selector5, selector6) {
         this.selector = selector;
         this.selector2 = selector2;
+        this.selector3 = selector3;
+        this.selector4 = selector4;
+        this.selector5 = selector5;
+        this.selector6 = selector6;
         this.data = data;
 
         this.init();
@@ -10,6 +14,7 @@ class RenderBalance {
     init() {
         this.generateItems();
         this.generateTotal();
+        this.renderSummary();
     }
 
     generateItems() {
@@ -71,6 +76,90 @@ class RenderBalance {
         return sumExpenses;
 
     }
+
+    leastIncome() {
+        let income = Infinity;
+        const count = this.data.account.length;
+        let month = 0;
+        for (let i = 0; i < count; i++) {
+            if (!this.data.account[i].income) {
+                Object.assign(this.data.account[i], {income: 0});
+            }
+            if (this.data.account[i].income < income && this.data.account[i].income !== 0) {
+                income = this.data.account[i].income;
+                month = i;
+            } else {
+                continue;
+            }
+        }
+        return month;
+    }
+
+    renderSummary() {
+        const DOM3 = document.querySelector(this.selector3);
+        DOM3.innerHTML = `${this.data.months[this.leastIncome()]}`;
+        const DOM4 = document.querySelector(this.selector4);
+        DOM4.innerHTML = `${this.data.months[this.mostIncome()]}`;
+        const DOM5 = document.querySelector(this.selector5);
+        DOM5.innerHTML = `${this.data.months[this.leastExpenses()]}`;
+        const DOM6 = document.querySelector(this.selector6);
+        DOM6.innerHTML = `${this.data.months[this.mostExpenses()]}`;
+    }
+
+    mostIncome() {
+        let income = -Infinity;
+        const count = this.data.account.length;
+        let month = 0;
+        for (let i = 0; i < count; i++) {
+            if (!this.data.account[i].income) {
+                Object.assign(this.data.account[i], {income: 0});
+            }
+            if (this.data.account[i].income > income) {
+                income = this.data.account[i].income;
+                month = i;
+            } else {
+                continue;
+            }
+        }
+        return month;
+    }
+
+    leastExpenses() {
+        let expenses = Infinity;
+        const count = this.data.account.length;
+        let month = 0;
+        for (let i = 0; i < count; i++) {
+            if (!this.data.account[i].expense) {
+                Object.assign(this.data.account[i], {expense: 0});
+            }
+            if (this.data.account[i].expense < expenses && this.data.account[i].expense !== 0) {
+                expenses = this.data.account[i].expense;
+                month = i;
+            } else {
+                continue;
+            }
+        }
+        return month;
+    }
+
+    mostExpenses() {
+        let expenses = -Infinity;
+        const count = this.data.account.length;
+        let month = 0;
+        for (let i = 0; i < count; i++) {
+            if (!this.data.account[i].expense) {
+                Object.assign(this.data.account[i], {expense: 0});
+            }
+            if (this.data.account[i].expense > expenses) {
+                expenses = this.data.account[i].expense;
+                month = i;
+            } else {
+                continue;
+            }
+        }
+        return month;
+    }
+
 }
 
 export { RenderBalance }
