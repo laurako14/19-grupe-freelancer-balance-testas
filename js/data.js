8740dd8a04d13c65e22c6ab7af56af935e7623c4
LauraKo <kovarskyte@yahoo.com>
2020-12-29 14:01:30 +0200

    Months

--- js/data.js
+++ js/data.js
@@ -12,3 +12,5 @@ const account = [
     { month: 8, income: 450, expense: 120 },
     { month: 12, expense: 200 }
 ];
+
+const months = [January, February, March, April, May, June, July, August, September, October, November, December];
